ffda7e43df759de8dc7f1acb14e0e345d5316332
noahehren <nehren@tulane.edu>
2022-05-04 16:52:45 -0500

    a

--- app.py
+++ app.py
@@ -34,6 +34,20 @@ st.markdown('''
     {
         padding: 1rem 2rem 2rem 2rem;
     }
+    .css-18e3th9 
+    {
+        padding-top: 0rem;
+        padding-bottom: 10rem;
+        padding-left: 5rem;
+        padding-right: 5rem;
+    }
+    .css-1d391kg 
+    {
+        padding-top: 3.5rem;
+        padding-right: 1rem;
+        padding-bottom: 3.5rem;
+        padding-left: 1rem;
+    }
 </style>
 ''', unsafe_allow_html=True)
 
