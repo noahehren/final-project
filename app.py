e3aa60d1c3d211bb1f43b8ba1d20831c3397c0d6
noahehren <nehren@tulane.edu>
2022-05-04 21:33:20 -0500

    a

--- app.py
+++ app.py
@@ -149,6 +149,7 @@ elif page_selected == 'Model':
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
         return X_train, X_test, y_train, y_test
     
+    @st.experimental_memo
     def plot_metrics(metrics_list):
         st.set_option('deprecation.showPyplotGlobalUse', False)
 
